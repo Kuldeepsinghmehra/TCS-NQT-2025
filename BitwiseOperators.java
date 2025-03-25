bae7ffed227f86c78689e92d15a714048d58fe16
kuldeepsmehra <kuldeepm272@gmail.com>
2025-03-25 14:50:00 +0530

    Added bitwise code concepts

--- BitwiseOperators.java
+++ BitwiseOperators.java
@@ -1,12 +1,35 @@
 public class BitwiseOperators {
 
     public static void main(String[] args) {
-        int p = 9;
+        int p = 12;
         int q = 10;
-        System.out.println("OR" + (p | q));
-        System.out.println("AND" + (p & q));
-        System.out.println("Left shift" + (p << q));
-        System.out.println("Right shift" + (p >> q));
-        System.out.println("XOR" + (p ^ q));
+        p=p^q;
+        q=p^q;
+        p=p^q;
+        // System.out.println("OR" + (p | q));
+        // System.out.println("AND" + (p & q));
+        // System.out.println("Left shift" + (p << q));
+        // System.out.println("Right shift" + (p >> q));
+        // System.out.println("XOR" + (p ^ q));
+
+        // TO Print Even or Odd
+  
+        // System.out.println("Number is "+((p&1)==0?"Even":"Odd"));
+
+
+        // Swapping of Numbers
+
+        // System.out.println("swapping"+p+" "+q);
+
+// to find unique Element only works for single unique elements
+
+//         int arr[] = {2, 3, 5, 3, 2};
+// int unique = 0;
+// for (int num : arr) {
+//     unique ^= num;
+// }
+// System.out.println("Unique Number: " + unique);
+
     }
+
 }
